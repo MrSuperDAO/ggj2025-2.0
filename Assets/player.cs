c3f197efd56386afacdd622b73fe76a2ba858fa9
alivite <1129839534@qq.com>
2025-01-16 23:08:12 +0800

    第三次test修改
    修改为3

--- Assets/player.cs
+++ Assets/player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int text = 2;
+    public int text = 3;
     void Start()
     {
         
