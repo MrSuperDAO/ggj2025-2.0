5fabbeff81e8caefac476a30ac7733257502a4a9
alivite <1129839534@qq.com>
2025-01-16 22:56:21 +0800

    修改test
    改为2

--- Assets/player.cs
+++ Assets/player.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     // Start is called before the first frame update
-<<<<<<< HEAD
     public int text = 2;
-=======
-    public int text = 99999;
->>>>>>> a47136ca5d35ec33b931e530cd95a1e2683191e0
     void Start()
     {
         
