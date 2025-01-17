fa22cbffbd3d12d99c251b263b6bd7cfca2a08a4
MrSuperDAO <1260482133@qq.com>
2025-01-17 22:41:48 +0800

    开始菜单+进入关卡

--- Assets/Gamescript/SceneChange.cs
+++ Assets/Gamescript/SceneChange.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject chooseLevelUI;
+
+
+    public void OpenChooseLevelUI()
     {
-        
+        chooseLevelUI.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CloseChooseLevelUI()
     {
-        
+        chooseLevelUI.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadLvel1()
+    {
+        SceneManager.LoadScene("level 1");
     }
 }
