3f62aa6927ffff84bb077446fb948014d2cf17ef
MrSuperDAO <1260482133@qq.com>
2025-01-18 11:41:35 +0800

    手感调整

--- Assets/Scprits/player/RotateTowardsMouse.cs
+++ Assets/Scprits/player/RotateTowardsMouse.cs
@@ -28,7 +28,7 @@ public class RotateTowardsMouse : MonoBehaviour
     {
         RotateNeedle();
         CheckPunctureBubble();
-        if (Input.GetMouseButtonDown(0) && currentBubble != null && !isBubblePunctured)
+        if (Input.GetMouseButtonDown(0) && currentBubble != null )
         {
             PunctureBubble();
         }
@@ -73,7 +73,7 @@ public class RotateTowardsMouse : MonoBehaviour
 
     private void PunctureBubble()
     {
-        Destroy(currentBubble); // 销毁泡泡
+        //Destroy(currentBubble); // 销毁泡泡
         isBubblePunctured = true;
         bubblePunctureTime = Time.time; // 记录戳破时间
 
