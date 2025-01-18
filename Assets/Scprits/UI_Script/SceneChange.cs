b23a88b5b326bac998e78256b5d224ae03c39bc7
MrSuperDAO <1260482133@qq.com>
2025-01-18 17:20:44 +0800

    更新合并

--- Assets/Scprits/UI_Script/SceneChange.cs
+++ Assets/Scprits/UI_Script/SceneChange.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class SceneChange : MonoBehaviour
 {
@@ -22,9 +24,39 @@ public class SceneChange : MonoBehaviour
     {
         Application.Quit();
     }
+    [SerializeField] Image transitionImage;
+    [SerializeField] public float fadeTime;
+    public static Color color;
 
     public void LoadLvel1()
     {
         SceneManager.LoadScene("level 1");
+       // StartCoroutine(TransitionOutCoroutine());
     }
+
+    public IEnumerator TransitionOutCoroutine()
+    {
+        transitionImage.gameObject.SetActive(true);//打开黑屏过场图片
+        transitionImage.color = new Color(transitionImage.color.r, transitionImage.color.g, transitionImage.color.b, 1f);
+        SceneManager.LoadScene("level 1");
+        
+        while (color.a > 0f)//如果加载完毕且完全黑屏，黑屏淡出
+        {
+            if (color.a <= 0.05f)
+            {
+                yield return null;
+                //屏蔽操作
+                //PlayerControl1.cantInput = false;
+            }
+
+            Time.timeScale = 1f;
+            color.a = Mathf.Clamp01(color.a - Time.unscaledDeltaTime / fadeTime);
+            transitionImage.color = color;
+            yield return null;
+        }
+        
+        transitionImage.gameObject.SetActive(false);//关闭黑屏过场图片
+        Destroy(this);
+    }
+
 }
